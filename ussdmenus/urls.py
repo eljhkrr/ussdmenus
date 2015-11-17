b4124a2f4fdcbf3f868c8ac243cd5dbb89b5509c
EL <eljhkrr@gmail.com>
2015-11-17 14:25:07 +0300

    view setup

--- ussdmenus/urls.py
+++ ussdmenus/urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls import urls
+from django.conf.urls import url
 
 from ussdmenus import views
 
 urlpatterns = [
+	url(r'', views.ussd_setup, name="ussd_setup"),
 	url(r'run', views.run, name="run"),
-];
+]
--- ussdmenus/views.py
+++ ussdmenus/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import xmlrpclib
 
-# Create your views here.
+def run(request):
+	params = {}
+	params['msisdn'] = request.POST['msisdn']
+	params['sessionId'] = request.POST['sessionId']
+	params['imsi'] = request.POST['imsi']
+	params['ussdRequestString'] = request.POST['ussdRequestString']
+	proxy = xmlrpclib.ServerProxy("http://127.0.0.1:15290/")
+	response = proxy.handleRequest(params)
+	response_text = '<pre>'+response['ussdResponseString']+'</pre>'
+	return HttpResponse(response_text)
+
+def ussd_setup(request):
+	return HttpResponse("")
