492f63b619447f5843a030d86976ab558f395c01
EL <eljhkrr@gmail.com>
2015-11-22 22:55:40 +0300

    appropriate error msg

--- ussdmenus/static/ussdmenus/js/app.js
+++ ussdmenus/static/ussdmenus/js/app.js
@@ -40,7 +40,7 @@ app.controller("AppController", ["$scope", "$http", function($scope, $http){
 		}, function errorCallback(response) {
 			// display notification
 			console.log("error sending");
-			$scope.ussdResponse = "<div class='text-danger text-center'><i class='glyphicon glyphicon-warning-sign'></i> Error sending request. <br>Is the socket <kbd>" + $scope.host.address + ":" + $scope.host.port +"</kbd> alive?</div>";
+			$scope.ussdResponse = "<div class='text-danger text-center'><i class='glyphicon glyphicon-warning-sign'></i> Error processing request. <br>Is the socket <kbd>" + $scope.host.address + ":" + $scope.host.port +"</kbd> alive?</div>";
 		});
 	};
 }]);
