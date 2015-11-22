fd74cf69298ee76dc96f0e89697bfbe3d186ee2a
EL <eljhkrr@gmail.com>
2015-11-22 20:48:45 +0300

    Getting USSD responses

--- ussdmenus/static/ussdmenus/js/app.js
+++ ussdmenus/static/ussdmenus/js/app.js
@@ -1,12 +1,38 @@
-var app = angular.module("ussd", ['ngWebsocket']);
+var app = angular.module("ussd", ['ngWebsocket', 'ngSanitize']);
 
-app.controller("AppController", ["$scope", function($scope){
+app.controller("AppController", ["$scope", "$http", function($scope, $http){
 
-	$scope.host = {"address": "1.2.3.4", "port": 123};
+	$scope.host = {"address": "127.0.0.1", "port": 15290};
+	$scope.query_string = "1";
+	$scope.ussdResponse = "<div class='muted text-center'>Response</div>";
 
 	$scope.clear = function(){
 		$scope.host = {"address": "", "port": null};
 	};
 
-	$scope.test = function(){};
+	$scope.test = function(){
+
+	};
+
+	$scope.send = function(){
+		console.log("send click");
+		var server = "http://" + $scope.host.address + ":" + $scope.host.port;
+		console.log(server);
+		var data = {"server": server, "query_string": $scope.query_string};
+		console.log("Request: ");
+		console.log(data);
+		$http({
+		  method: 'GET',
+		  url: '/ussdmenus/run',
+		  params: data
+		}).then(function successCallback(response) {
+			// update UI with response
+			console.log(response.data);
+			$scope.ussdResponse = response.data;
+			//$scope.$apply();
+		}, function errorCallback(response) {
+			// display notification
+			console.log("error sending");
+		});
+	};
 }]);
--- ussdmenus/views.py
+++ ussdmenus/views.py
@@ -9,18 +9,29 @@ from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie, csrf_p
 #@requires_csrf_token
 def run(request):
 	params = {}
-	# params['msisdn'] = request.GET['msisdn']
-	# params['sessionId'] = request.GET['sessionId']
-	# params['imsi'] = request.GET['imsi']
-	# params['ussdRequestString'] = request.GET['ussdRequestString']
-	#host_port = 
+	# params['msisdn'] = request.POST['msisdn']
+	# params['sessionId'] = request.POST['sessionId']
+	# params['imsi'] = request.POST['imsi']
+	# params['ussdRequestString'] = request.POST['query_string']
+	# host_port = 
 	params['msisdn'] = "2343243424"
 	params['sessionId'] = "23432342"
 	params['imsi'] = "23423423"
-	params['ussdRequestString'] = "2"
-	proxy = xmlrpclib.ServerProxy("http://127.0.0.1:15290")
+	params['ussdRequestString'] = request.GET["query_string"]
+	proxy = xmlrpclib.ServerProxy(str(request.GET["server"]))
 	response = proxy.handleRequest(params)
-	response_text = '<pre>'+response['ussdResponseString']+'</pre>'
+	response_text = '<pre>' + response['ussdResponseString'] + '</pre>'
+	#response_text = dict(request.body)
+	# response_text = type(request.body)
+	# response_text = request.GET["query_string"]
+	# response_text = request.GET["server"]
+	#response_text = dict(request)
+	#response_text = request.get("server", default="No Server")
+	# if request.GET:
+	# 	response_text = "Theres GET data"
+	# else:
+	# 	response_text = "No GET data"
+
 	return HttpResponse(response_text)
 	#r = request.POST
 	#return HttpResponse(r)
